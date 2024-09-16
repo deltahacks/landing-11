3f10ce3ac0c439d338fc0937017015fdc9c558dc
Krish120003 <krish120003@gmail.com>
2024-09-16 17:27:36 -0400

    feat: add footer image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Sponsors from "~/sections/Sponsors";
 import FAQ from "~/sections/FAQ";
+import Footer from "~/sections/Footer";
 
 export default function Home() {
   return (
-    <main className="bg-violet-200">
+    <main className="bg-[#EFDDFE]">
       hi
       <Sponsors />
       <FAQ />
+      <Footer />
     </main>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="grid min-h-screen grid-cols-1 grid-rows-1 bg-red-500">
+      <div className="grid-col relative col-start-1 row-start-1 overflow-clip object-cover p-16">
+        <Image
+          src={"/footer_low.png"}
+          alt={"Footer Blob Image"}
+          objectFit="cover"
+          width={1920}
+          height={1080}
+        />
+      </div>
+      <div className="z-10 col-start-1 row-start-1">
+        <p>HELLO</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
