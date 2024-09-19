8e45fe15f953cb407a30046a78b3a91b1217777b
Selina <selinakim343@outlook.com>
2024-09-19 19:11:38 -0400

    feat: add hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Sponsors from "~/sections/Sponsors";
 import FAQ from "~/sections/FAQ";
 import Footer from "~/sections/Footer";
+import Hero from "~/sections/Hero";
 
 export default function Home() {
   return (
     <main className="bg-[#EFDDFE]">
-      hi
+      <Hero />
       <Sponsors />
       <FAQ />
       <Footer />
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>hero</>
+  );
+};
+
+export default Hero;
