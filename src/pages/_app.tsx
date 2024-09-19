0fe6af356b79348e35dde14fb91befbf2085f0a7
Krish120003 <krish120003@gmail.com>
2024-09-19 13:56:22 -0400

    fix: use null coalescing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ import { PostHogProvider } from "posthog-js/react";
 if (typeof window !== "undefined") {
   // checks that we are client-side
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    api_host: env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
     ui_host: "https://app.posthog.com",
     loaded: (posthog) => {
