530cc35e8776e05126fdef0ea99c614203ee3c5a
Rachelle DeMan <demanr@mcmaster.ca>
2024-09-19 23:17:39 -0400

    feat: base mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { Drawer } from "vaul";
 //import "./Navbar.css"; // Assuming you have a CSS file for styling
 
 const Navbar: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const hideSidebar = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="sticky top-0 z-50 flex h-full w-full items-center justify-between gap-4 px-6 py-5 backdrop-blur-xl md:px-8">
+    <div className="sticky top-0 z-40 flex h-full w-full items-center justify-between gap-4 px-6 py-5 backdrop-blur-lg md:px-8">
       <div className="flex h-full w-full lg:gap-6 lg:px-4 xl:gap-14">
-        <div className="md:flex md:items-center md:gap-4">
-          <Link href="/" className="">
+        {/* {!open && ( */}
+        <div className="z-0 md:flex md:items-center md:gap-4">
+          <Link href="/">
             <Image
               width="68"
               height="68"
-              className="h-18"
+              className="h-18 z-0"
               src="/nav_logo.svg"
               alt="logo"
             />
           </Link>
         </div>
+        {/* )} */}
         <div className="hidden w-fit items-center justify-between gap-6 px-12 text-white md:flex md:text-xl lg:px-0 lg:text-2xl xl:gap-12">
           <Link href="#WhoAreWe">About</Link>
           <Link href="#Community">Statistics</Link>
@@ -67,70 +75,98 @@ const Navbar: React.FC = () => {
           />
         </Link>
       </div>
-      <div className="inset-0 md:hidden">
-        <Image width="24" height="24" src="/nav_sizing.svg" alt="NAV" />
-        <div className="drawer-side h-[calc(100dvh)] w-screen overscroll-none">
-          <div className="flex flex-col justify-between">
-            <div className="text-white">
-              <li>
-                <Link href="#">Home</Link>
-              </li>
-              <li>
-                <Link href="#WhoAreWe">About</Link>
-              </li>
-              <li>
-                <Link href="#Community">Statistics</Link>
-              </li>
-              <li>
-                <Link href="#Carousel">Past Events</Link>
-              </li>
-              <li>
-                <Link href="#Sponsers">Sponsors</Link>
-              </li>
-              <li>
-                <Link href="#FAQ">FAQ</Link>
-              </li>
-            </div>
-            <div className="align-center flex w-full items-center justify-center gap-4 pt-24 md:gap-4">
-              <Link href="https://www.linkedin.com/company/deltahacks/mycompany/">
+      <div className="inset-0 z-50 md:hidden">
+        <Drawer.Root direction="right" open={open} onOpenChange={setOpen}>
+          <Drawer.Trigger asChild>
+            <button className="">
+              {!open && (
                 <Image
-                  className=""
-                  width="36"
-                  height="36"
-                  src="/nav_linkedin.svg"
-                  alt="logo"
+                  width="24"
+                  height="24"
+                  src="/nav_sizing.svg"
+                  className="w-full"
+                  alt="NAV"
                 />
-              </Link>
-              <Link href="https://www.instagram.com/deltahacks">
-                <Image
-                  className=""
-                  width="36"
-                  height="36"
-                  src="/nav_insta.svg"
-                  alt="Instagram"
-                />
-              </Link>
-              <Link href="https://www.instagram.com/deltahacks">
-                <Image
-                  className=""
-                  width="36"
-                  height="36"
-                  src="/nav_devpost.svg"
-                  alt="Devpost"
-                />
-              </Link>
-              <Link href="https://www.tiktok.com/@deltahacks">
-                <Image
-                  className=""
-                  width="36"
-                  height="36"
-                  src="/nav_tiktak.svg"
-                  alt="TikTok"
-                />
-              </Link>
-            </div>
-          </div>
-        </div>
+              )}
+            </button>
+          </Drawer.Trigger>
+          <Drawer.Portal>
+            <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+            <Drawer.Content className="fixed bottom-0 right-0 mt-24 flex h-full w-full flex-col rounded-t-[10px] bg-white">
+              <div className="absolute right-0 top-0 p-8 pt-10">
+                {open && (
+                  <Image
+                    width="24"
+                    height="24"
+                    src="/nav_close.svg"
+                    alt="CLOSE"
+                    className=""
+                    onClick={hideSidebar}
+                  />
+                )}
+              </div>
+              <div className="drawer-side h-[calc(100dvh)] w-screen overscroll-none bg-[#644A93]">
+                <div className="flex h-full flex-col justify-between px-16">
+                  <div className="flex w-fit flex-col gap-8 pt-28 text-xl text-white">
+                    <Link href="/" onClick={hideSidebar}>
+                      About
+                    </Link>
+                    <Link href="#Community" onClick={hideSidebar}>
+                      Statistics
+                    </Link>
+                    <Link href="#Carousel" onClick={hideSidebar}>
+                      Past Events
+                    </Link>
+                    <Link href="#Sponsers" onClick={hideSidebar}>
+                      Sponsors
+                    </Link>
+                    <Link href="#FAQ" onClick={hideSidebar}>
+                      FAQ
+                    </Link>
+                  </div>
+                  <div className="align-center flex w-full items-center justify-center gap-4 py-16 md:gap-4">
+                    <Link href="https://www.linkedin.com/company/deltahacks/mycompany/">
+                      <Image
+                        className=""
+                        width="36"
+                        height="36"
+                        src="/nav_linkedin.svg"
+                        alt="logo"
+                      />
+                    </Link>
+                    <Link href="https://www.instagram.com/deltahacks">
+                      <Image
+                        className=""
+                        width="36"
+                        height="36"
+                        src="/nav_insta.svg"
+                        alt="Instagram"
+                      />
+                    </Link>
+                    <Link href="https://www.instagram.com/deltahacks">
+                      <Image
+                        className=""
+                        width="36"
+                        height="36"
+                        src="/nav_devpost.svg"
+                        alt="Devpost"
+                      />
+                    </Link>
+                    <Link href="https://www.tiktok.com/@deltahacks">
+                      <Image
+                        className=""
+                        width="36"
+                        height="36"
+                        src="/nav_tiktak.svg"
+                        alt="TikTok"
+                      />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </Drawer.Content>
+          </Drawer.Portal>
+        </Drawer.Root>
       </div>
     </div>
   );
