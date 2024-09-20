c6d0279b7a546d50e8a2670e869338deed0f2a69
Rachelle DeMan <demanr@mcmaster.ca>
2024-09-19 21:35:16 -0400

    feat: fixing up medium sized view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,36 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 //import "./Navbar.css"; // Assuming you have a CSS file for styling
 
 const Navbar: React.FC = () => {
-  const hideSidebar = (e: any) => {
-    // document.getElementById("my-drawer-4").checked = false;
-    // // continue the event
-    return true;
-  };
-
   return (
-    <div className="flex h-full w-full items-center justify-between bg-[#19333A] bg-opacity-40 px-6 py-5 backdrop-blur-xl md:px-8">
-      <div className="flex h-full w-full gap-6 px-4 xl:gap-14">
-        <a href="#Hero" className="">
-          <Image
-            width="68"
-            height="68"
-            className="h-18"
-            src="/nav_logo.svg"
-            alt="logo"
-          />
-        </a>
-        <div className="hidden w-fit items-center justify-between gap-6 px-12 text-white md:flex md:text-2xl lg:px-0 xl:gap-12">
-          <a href="#WhoAreWe">About</a>
-          <a href="#Community">Statistics</a>
-          <a href="#Carousel">Past Events</a>
-          <a href="#Sponsors">Sponsors</a>
-          <a href="#FAQ">FAQ</a>
+    <div className="flex h-full w-full items-center justify-between gap-4 bg-[#19333A] bg-opacity-40 px-6 py-5 backdrop-blur-xl md:px-8">
+      <div className="flex h-full w-full lg:gap-6 lg:px-4 xl:gap-14">
+        <div className="md:flex md:items-center md:gap-4">
+          <Link href="/" className="">
+            <Image
+              width="68"
+              height="68"
+              className="h-18"
+              src="/nav_logo.svg"
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <div className="hidden w-fit items-center justify-between gap-6 px-12 text-white md:flex md:text-xl lg:px-0 lg:text-2xl xl:gap-12">
+          <Link href="#WhoAreWe">About</Link>
+          <Link href="#Community">Statistics</Link>
+          <Link className="min-w-fit" href="#Carousel">
+            Past Events
+          </Link>
+          <Link href="#Sponsors">Sponsors</Link>
+          <Link href="#">FAQ</Link>
         </div>
       </div>
       <div className="hidden md:flex md:items-center md:gap-4">
-        <a href="https://www.linkedin.com/company/deltahacks/mycompany/">
+        <Link href="https://www.linkedin.com/company/deltahacks/mycompany/">
           <Image
             className=""
             width="36"
@@ -39,8 +38,8 @@ const Navbar: React.FC = () => {
             src="/nav_linkedin.svg"
             alt="logo"
           />
-        </a>
-        <a href="https://www.instagram.com/deltahacks">
+        </Link>
+        <Link href="https://www.instagram.com/deltahacks">
           <Image
             className=""
             width="36"
@@ -48,8 +47,8 @@ const Navbar: React.FC = () => {
             src="/nav_insta.svg"
             alt="Instagram"
           />
-        </a>
-        <a href="https://www.instagram.com/deltahacks">
+        </Link>
+        <Link href="https://www.instagram.com/deltahacks">
           <Image
             className=""
             width="36"
@@ -57,8 +56,8 @@ const Navbar: React.FC = () => {
             src="/nav_devpost.svg"
             alt="Devpost"
           />
-        </a>
-        <a href="https://www.instagram.com/deltahacks">
+        </Link>
+        <Link href="https://www.tiktok.com/@deltahacks">
           <Image
             className=""
             width="36"
@@ -66,91 +65,70 @@ const Navbar: React.FC = () => {
             src="/nav_tiktak.svg"
             alt="TikTok"
           />
-        </a>
+        </Link>
       </div>
       <div className="inset-0 md:hidden">
-        <div className="drawer drawer-end">
-          <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content">
-            {/* Page content here */}
-            <label htmlFor="my-drawer-4" className="drawer-button">
-              <Image width="24" height="24" src="/nav_sizing.svg" alt="NAV" />
-            </label>
-          </div>
-          <div className="drawer-side h-[calc(100dvh)] w-screen overscroll-none">
-            <ul className="menu text-base-content h-[calc(100dvh)] min-h-full w-screen gap-4 bg-[#19333A] p-4 px-6 text-xl">
-              <label
-                htmlFor="my-drawer-4"
-                aria-label="close sidebar"
-                className="align-center drawer-overlay flex w-full justify-end pb-16"
-              >
-                <Image width="24" height="24" src="/nav_close.svg" alt="NAV" />
-              </label>
-              {/* <!-- <input id="my-drawer-4" type="checkbox" className="drawer-toggle" /> --> */}
-              <div className="flex flex-col justify-between">
-                <div className="text-white">
-                  <li>
-                    <a href="#" onClick={hideSidebar}>
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#WhoAreWe" onClick={hideSidebar}>
-                      About
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#Community" onClick={hideSidebar}>
-                      Statistics
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#Carousel" onClick={hideSidebar}>
-                      Past Events
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#Sponsers" onClick={hideSidebar}>
-                      Sponsors
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#FAQ" onClick={hideSidebar}>
-                      FAQ
-                    </a>
-                  </li>
-                </div>
-                <div className="align-center flex w-full items-center justify-center gap-4 pt-24 md:gap-4">
-                  <a href="https://www.linkedin.com/company/deltahacks/mycompany/">
-                    <Image
-                      className=""
-                      width="72"
-                      height="72"
-                      src="/nav_linkedin.svg"
-                      alt="logo"
-                    />
-                  </a>
-                  <a href="https://www.instagram.com/deltahacks">
-                    <Image
-                      className=""
-                      width="72"
-                      height="72"
-                      src="/nav_insta.svg"
-                      alt="Instagram"
-                    />
-                  </a>
-                  <a href="https://twitter.com/deltaHacks">
-                    <Image
-                      className=""
-                      width="72"
-                      height="72"
-                      src="/nav_twitter.svg"
-                      alt="Twitter"
-                    />
-                  </a>
-                </div>
-              </div>
-            </ul>
+        <Image width="24" height="24" src="/nav_sizing.svg" alt="NAV" />
+        <div className="drawer-side h-[calc(100dvh)] w-screen overscroll-none">
+          <div className="flex flex-col justify-between">
+            <div className="text-white">
+              <li>
+                <Link href="#">Home</Link>
+              </li>
+              <li>
+                <Link href="#WhoAreWe">About</Link>
+              </li>
+              <li>
+                <Link href="#Community">Statistics</Link>
+              </li>
+              <li>
+                <Link href="#Carousel">Past Events</Link>
+              </li>
+              <li>
+                <Link href="#Sponsers">Sponsors</Link>
+              </li>
+              <li>
+                <Link href="#FAQ">FAQ</Link>
+              </li>
+            </div>
+            <div className="align-center flex w-full items-center justify-center gap-4 pt-24 md:gap-4">
+              <Link href="https://www.linkedin.com/company/deltahacks/mycompany/">
+                <Image
+                  className=""
+                  width="36"
+                  height="36"
+                  src="/nav_linkedin.svg"
+                  alt="logo"
+                />
+              </Link>
+              <Link href="https://www.instagram.com/deltahacks">
+                <Image
+                  className=""
+                  width="36"
+                  height="36"
+                  src="/nav_insta.svg"
+                  alt="Instagram"
+                />
+              </Link>
+              <Link href="https://www.instagram.com/deltahacks">
+                <Image
+                  className=""
+                  width="36"
+                  height="36"
+                  src="/nav_devpost.svg"
+                  alt="Devpost"
+                />
+              </Link>
+              <Link href="https://www.tiktok.com/@deltahacks">
+                <Image
+                  className=""
+                  width="36"
+                  height="36"
+                  src="/nav_tiktak.svg"
+                  alt="TikTok"
+                />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
@@ -159,3 +137,83 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+
+//  <div className="drawer drawer-end">
+//           <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+//           <div className="drawer-content">
+//             {/* Page content here */}
+//             <label htmlFor="my-drawer-4" className="drawer-button">
+//               <Image width="24" height="24" src="/nav_sizing.svg" alt="NAV" />
+//             </label>
+//           </div>
+//           <div className="drawer-side h-[calc(100dvh)] w-screen overscroll-none">
+//             <ul className="menu text-base-content h-[calc(100dvh)] min-h-full w-screen gap-4 bg-[#19333A] p-4 px-6 text-xl">
+//               <label
+//                 htmlFor="my-drawer-4"
+//                 aria-label="close sidebar"
+//                 className="align-center drawer-overlay flex w-full justify-end pb-16"
+//               >
+//                 <Image width="24" height="24" src="/nav_close.svg" alt="NAV" />
+//               </label>
+//               {/* <!-- <input id="my-drawer-4" type="checkbox" className="drawer-toggle" /> --> */}
+//               <div className="flex flex-col justify-between">
+//                 <div className="text-white">
+//                   <li>
+//                     <Link href="#" onClick={hideSidebar}>
+//                       Home
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href="#WhoAreWe" onClick={hideSidebar}>
+//                       About
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href="#Community" onClick={hideSidebar}>
+//                       Statistics
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href="#Carousel" onClick={hideSidebar}>
+//                       Past Events
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href="#Sponsers" onClick={hideSidebar}>
+//                       Sponsors
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href="#FAQ" onClick={hideSidebar}>
+//                       FAQ
+//                     </Link>
+//                   </li>
+//                 </div>
+//                 <div className="align-center flex w-full items-center justify-center gap-4 pt-24 md:gap-4">
+//                   <Link href="https://www.linkedin.com/company/deltahacks/mycompany/">
+//                     <Image
+//                       className=""
+//                       width="72"
+//                       height="72"
+//                       src="/nav_linkedin.svg"
+//                       alt="logo"
+//                     />
+//                   </Link>
+//                   <Link href="https://www.instagram.com/deltahacks">
+//                     <Image
+//                       className=""
+//                       width="72"
+//                       height="72"
+//                       src="/nav_insta.svg"
+//                       alt="Instagram"
+//                     />
+//                   </Link>
+//                   <Link href="https://twitter.com/deltaHacks">
+//                     <Image
+//                       className=""
+//                       width="72"
+//                       height="72"
+//                       src="/nav_twitter.svg"
+//                       alt="Twitter"
+//                     />
+//                   </Link>
