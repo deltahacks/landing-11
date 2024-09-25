3eac50016076128b9c4144ac42b210a5e62c086f
Krish120003 <krish120003@gmail.com>
2024-09-24 20:37:26 -0400

    fix: update profile settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ if (typeof window !== "undefined") {
   // checks that we are client-side
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
     ui_host: "https://app.posthog.com",
     loaded: (posthog) => {
       if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
