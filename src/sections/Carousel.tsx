4a1168c1665df53064f85eaf8eedac23f0271d27
Owen Gretzinger <owengretzinger@gmail.com>
2024-10-02 16:59:10 -0400

    fix layout & make layout responsive

--- src/sections/Carousel.tsx
+++ src/sections/Carousel.tsx
@@ -1,55 +1,61 @@
+import Image from "next/image";
 import React from "react";
 import ImageGallery, { type ReactImageGalleryItem } from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 // Images
-const images: ReactImageGalleryItem[] = await Promise.all(
-  [
-    {
-      original: "/carousel/DSC01792.jpeg",
-      originalAlt: "A1",
-      description: "D1",
-    },
-    {
-      original: "/carousel/DSC02061.jpeg",
-      originalAlt: "A2",
-      description: "D2",
-    },
-    {
-      original: "/carousel/DSCF1862.jpeg",
-      originalAlt: "A3",
-      description: "D3",
-    },
-    {
-      original: "/carousel/DSCF1873.jpeg",
-      originalAlt: "A4",
-      description: "D4",
-    },
-    {
-      original: "/carousel/DSCF2187.jpeg",
-      originalAlt: "A5",
-      description: "D5",
-    },
-  ].map((image) => {
-    return {
-      ...image,
-      originalHeight: 900,
-      originalWidth: 1600,
-    };
-  }),
-);
+const images: ReactImageGalleryItem[] = [
+  {
+    original: "/carousel/DSC01792.jpeg",
+    originalAlt: "A1",
+    description: "D1",
+  },
+  {
+    original: "/carousel/DSC02061.jpeg",
+    originalAlt: "A2",
+    description: "D2",
+  },
+  {
+    original: "/carousel/DSCF1862.jpeg",
+    originalAlt: "A3",
+    description: "D3",
+  },
+  {
+    original: "/carousel/DSCF1873.jpeg",
+    originalAlt: "A4",
+    description: "D4",
+  },
+  {
+    original: "/carousel/DSCF2187.jpeg",
+    originalAlt: "A5",
+    description: "D5",
+  },
+].map((image) => {
+  return {
+    ...image,
+    originalHeight: 540,
+    originalWidth: 780,
+  };
+});
 
 // Carousel
 const Carousel = () => {
   return (
-    <div>
-      <p className="mb-10 text-center font-display text-8xl font-bold text-white drop-shadow-2xl">
+    <div className="flex w-full flex-col items-center overflow-hidden">
+      {/* replace with header component once PR #17 is merged */}
+      <p className="mb-10 text-center font-display text-3xl font-bold text-white drop-shadow-2xl lg:text-8xl">
         WHAT&apos;S TO COME...
       </p>
-      <div className="relative mx-auto h-[1081px] max-w-[1231px]">
-        <div className="absolute inset-0 w-full bg-[url('/carousel/carousel-house.png')] bg-center bg-no-repeat"></div>
-        <div className="z-1 absolute inset-0 flex flex-col items-center justify-end">
-          <div className="mb-[6%] ml-[2%] w-2/3 overflow-hidden rounded-md shadow-md">
+      <div className="relative mr-4 w-fit lg:mr-10"> {/* add some right margin since the graphic is not symmetrical */}
+        <Image
+          src="/carousel/carousel-house.png"
+          width={1227}
+          height={1080}
+          alt="House"
+          className="aspect-[1231/1080] w-[448px] min-w-[448px] lg:w-[1231px] lg:min-w-[1231px]"
+        />
+        <div className="absolute bottom-4 left-1.5 flex w-full justify-center lg:bottom-10 lg:left-6">
+          <div className="">
             <ImageGallery
               items={images}
               showThumbnails={false}
@@ -57,6 +63,18 @@ const Carousel = () => {
               autoPlay={true}
               slideInterval={5000}
               lazyLoad={true}
+              renderItem={(item: ReactImageGalleryItem) => (
+                <Image
+                  // getting random type errors for some reason
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+                  src={item.original}
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+                  alt={item.originalAlt || "DeltaHacks Carousel Image"}
+                  width={754}
+                  height={536}
+                  className="aspect-[780/540] w-[280px] object-cover lg:w-[780px]"
+                />
+              )}
             />
           </div>
         </div>
