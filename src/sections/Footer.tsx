5205816f3f6c1cec5cec9061706d43dacd1d641f
Jason Huang <jasonhuang20035@gmail.com>
2024-10-03 16:44:27 -0400

    feat: add footer social icons

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 import snowman from "~/assets/footer/snowman.png";
 import Link from "next/link";
 import dynamic from "next/dynamic";
+import { RiInstagramLine, RiTiktokLine } from "react-icons/ri";
+import { FaLinkedin } from "react-icons/fa6";
+import { SiDevpost } from "react-icons/si";
+import { FaTiktok } from "react-icons/fa6";
 
 const TeamMarquee = dynamic(() => import("~/components/TeamMarquee"), {
   ssr: false,
@@ -16,7 +20,17 @@ const Footer = () => {
         Made with ♥ from the DeltaHacks Team
       </p>
       <TeamMarquee />
-      <div className="mt-32 flex w-full max-w-screen-lg flex-col gap-3 py-12 text-center text-xl lg:flex-row lg:gap-8 lg:border-t lg:border-black">
+      <div className="mt-20 flex w-full max-w-screen-lg flex-col items-center gap-3 pb-4 text-xl lg:flex-row lg:gap-8 lg:border-t lg:border-black lg:py-10">
+        <div className="flex flex-row gap-1 pb-4 text-white lg:hidden">
+          <RiInstagramLine className="size-8" />
+          {/* There is no Material UI icon set in react-icons, 
+          this icon appears to be similar enough */}
+          <FaLinkedin className="size-8" />
+          <SiDevpost className="size-8" />
+          {/* There is no Iconify icon set in react-icons, 
+          this icon appears to be similar enough */}
+          <FaTiktok className="size-8" />
+        </div>
         <Link
           href="https://github.com/DeltaHacks/deltahacks-website-2024"
           className="hover:underline"
