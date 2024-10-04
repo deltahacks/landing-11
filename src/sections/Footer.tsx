38c625ce84d0c04008889afe81f8a65dae7e55bc
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-10-04 16:20:13 -0400

    fix: make newsletter a section instead of component

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,14 +1,129 @@
 import React from "react";
 import Image from "next/image";
-import Newsletter from "~/components/Newsletter";
 
 const Footer = () => {
   return (
     <footer className="grid min-h-screen grid-cols-1 grid-rows-1 bg-red-500">
       <div className="grid-col relative col-start-1 row-start-1 overflow-clip object-cover p-16">
-        {/* <div className="z-[-1000] mt-32">
-          <Newsletter />
-        </div> */}
+        <div className="mt-48">
+          <div className="relative mx-auto mt-10 max-w-2xl">
+            <div className="letter absolute left-1/2 right-0 z-0 w-3/4 -translate-x-1/2 transform cursor-pointer md:bottom-48">
+              <Image
+                src={"/letter.svg"}
+                alt={"Newsletter Letter"}
+                width={1000}
+                height={1000}
+              />
+            </div>
+            <div className="relative z-10 mx-auto mt-10 max-w-2xl rounded-lg bg-white p-6 shadow-md">
+              <div id="mc_embed_signup">
+                <form
+                  action="https://deltahacks.us19.list-manage.com/subscribe/post?u=7ac221f02edc4242db4789d8f&amp;id=380c22c749&amp;f_id=00a736e7f0"
+                  method="post"
+                  id="mc-embedded-subscribe-form"
+                  name="mc-embedded-subscribe-form"
+                  className="validate"
+                  target="_self"
+                  noValidate
+                >
+                  <div
+                    id="mc_embed_signup_scroll"
+                    className="flex flex-col items-center justify-center space-y-4"
+                  >
+                    <h2 className="text-violet-900">
+                      Follow us on social media to stay up to date on latest
+                      news about DeltaHacks!
+                    </h2>
+                    <div className="mt-4 flex justify-center space-x-4">
+                      <a href="https://www.instagram.com/deltahacks/?hl=en">
+                        <Image
+                          src={"/instagram.svg"}
+                          alt={"Social Media Icons"}
+                          width={36}
+                          height={36}
+                        />
+                      </a>
+
+                      <a href="https://deltahacks-x.devpost.com/">
+                        <Image
+                          src={"/devpost.svg"}
+                          alt={"Social Media Icons"}
+                          width={36}
+                          height={36}
+                        />
+                      </a>
+
+                      <a href="https://www.tiktok.com/@deltahacks">
+                        <Image
+                          src={"/tiktok.svg"}
+                          alt={"Social Media Icons"}
+                          width={36}
+                          height={36}
+                        />
+                      </a>
+                    </div>
+                    <h3 className="mb-4 text-center text-xl text-violet-900">
+                      Sign up to our newsletter!
+                    </h3>
+
+                    <div className="flex w-full flex-col space-y-4 px-20 sm:flex-row sm:space-x-4 sm:space-y-0">
+                      <div className="flex-grow">
+                        <div className="mc-field-group">
+                          <label
+                            htmlFor="mce-EMAIL"
+                            className="mb-1 block text-sm font-medium text-gray-700"
+                          >
+                            Email Address
+                          </label>
+                          <input
+                            type="email"
+                            name="EMAIL"
+                            className="required email w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-violet-500"
+                            id="mce-EMAIL"
+                            required={true}
+                          />
+                        </div>
+                        <div className="hidden">
+                          <input type="hidden" name="tags" value="24287902" />
+                        </div>
+                        <div id="mce-responses" className="clear">
+                          <div
+                            className="response hidden"
+                            id="mce-error-response"
+                          ></div>
+                          <div
+                            className="response hidden"
+                            id="mce-success-response"
+                          ></div>
+                        </div>
+                        <div
+                          aria-hidden="true"
+                          className="absolute left-[-5000px]"
+                        >
+                          <input
+                            type="text"
+                            name="b_7ac221f02edc4242db4789d8f_380c22c749"
+                            tabIndex={-1}
+                            value=""
+                          />
+                        </div>
+                      </div>
+                      <div className="flex items-end">
+                        <input
+                          type="submit"
+                          name="subscribe"
+                          id="mc-embedded-subscribe"
+                          className="w-full cursor-pointer rounded-md bg-violet-900 px-4 py-2 font-bold text-white transition duration-300 ease-in-out hover:bg-violet-600 sm:w-auto"
+                          value="Sign up!"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
         <Image
           src={"/footer_low.png"}
           alt={"Footer Blob Image"}
@@ -17,9 +132,6 @@ const Footer = () => {
           height={1080}
         />
       </div>
-      {/* <div className="z-10 col-start-1 row-start-1">
-        <p>HELLO</p>
-      </div> */}
     </footer>
   );
 };
