9c1ab8f82003e82dea6670cdf34b9ea0de283ee5
Rachelle DeMan <demanr@mcmaster.ca>
2024-10-05 01:30:37 -0400

    chore: removing comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,6 @@ import Link from "next/link";
 import { Drawer } from "vaul";
 import clsx from "clsx";
 
-//import "./Navbar.css"; // Assuming you have a CSS file for styling
-
 const Navbar: React.FC = () => {
   const [open, setOpen] = useState(false);
   const [container, setContainer] = useState(null);
@@ -38,9 +36,7 @@ const Navbar: React.FC = () => {
         },
       )}
     >
-      {/* backdrop-blur-md */}
       <div className="flex h-full w-full lg:gap-6 lg:px-4 xl:gap-14">
-        {/* {!open && ( */}
         <div className="z-0 flex md:flex md:items-center md:gap-4">
           <Link href="/">
             <Image
@@ -52,7 +48,6 @@ const Navbar: React.FC = () => {
             />
           </Link>
         </div>
-        {/* )} */}
         <div className="hidden w-fit items-center justify-between gap-6 px-12 text-white md:flex md:text-xl lg:px-0 lg:text-2xl xl:gap-12">
           <Link href="#WhoAreWe">About</Link>
           <Link href="#Community">Statistics</Link>
@@ -102,15 +97,11 @@ const Navbar: React.FC = () => {
         </Link>
       </div>
       <div className="inset-0 z-40 md:invisible md:hidden">
-        {/* <div className="z-40">
-          <div ref={setContainer} />
-        </div> */}
         <Drawer.Root
           direction="right"
           open={open}
           onOpenChange={setOpen}
           noBodyStyles={true}
-          // container={container}
         >
           <Drawer.Trigger asChild>
             <button className="">
@@ -208,83 +199,3 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
-
-//  <div className="drawer drawer-end">
-//           <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-//           <div className="drawer-content">
-//             {/* Page content here */}
-//             <label htmlFor="my-drawer-4" className="drawer-button">
-//               <Image width="24" height="24" src="/nav_sizing.svg" alt="NAV" />
-//             </label>
-//           </div>
-//           <div className="drawer-side h-[calc(100dvh)] w-screen overscroll-none">
-//             <ul className="menu text-base-content h-[calc(100dvh)] min-h-full w-screen gap-4 bg-[#19333A] p-4 px-6 text-xl">
-//               <label
-//                 htmlFor="my-drawer-4"
-//                 aria-label="close sidebar"
-//                 className="align-center drawer-overlay flex w-full justify-end pb-16"
-//               >
-//                 <Image width="24" height="24" src="/nav_close.svg" alt="NAV" />
-//               </label>
-//               {/* <!-- <input id="my-drawer-4" type="checkbox" className="drawer-toggle" /> --> */}
-//               <div className="flex flex-col justify-between">
-//                 <div className="text-white">
-//                   <li>
-//                     <Link href="#" onClick={hideSidebar}>
-//                       Home
-//                     </Link>
-//                   </li>
-//                   <li>
-//                     <Link href="#WhoAreWe" onClick={hideSidebar}>
-//                       About
-//                     </Link>
-//                   </li>
-//                   <li>
-//                     <Link href="#Community" onClick={hideSidebar}>
-//                       Statistics
-//                     </Link>
-//                   </li>
-//                   <li>
-//                     <Link href="#Carousel" onClick={hideSidebar}>
-//                       Past Events
-//                     </Link>
-//                   </li>
-//                   <li>
-//                     <Link href="#Sponsers" onClick={hideSidebar}>
-//                       Sponsors
-//                     </Link>
-//                   </li>
-//                   <li>
-//                     <Link href="#FAQ" onClick={hideSidebar}>
-//                       FAQ
-//                     </Link>
-//                   </li>
-//                 </div>
-//                 <div className="align-center flex w-full items-center justify-center gap-4 pt-24 md:gap-4">
-//                   <Link href="https://www.linkedin.com/company/deltahacks/mycompany/">
-//                     <Image
-//                       className=""
-//                       width="72"
-//                       height="72"
-//                       src="/nav_linkedin.svg"
-//                       alt="logo"
-//                     />
-//                   </Link>
-//                   <Link href="https://www.instagram.com/deltahacks">
-//                     <Image
-//                       className=""
-//                       width="72"
-//                       height="72"
-//                       src="/nav_insta.svg"
-//                       alt="Instagram"
-//                     />
-//                   </Link>
-//                   <Link href="https://twitter.com/deltaHacks">
-//                     <Image
-//                       className=""
-//                       width="72"
-//                       height="72"
-//                       src="/nav_twitter.svg"
-//                       alt="Twitter"
-//                     />
-//                   </Link>
