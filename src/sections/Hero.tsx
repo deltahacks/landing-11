11c2adade867dc35645b965e850ddaaf58a4c650
Madhu Sivapragasam <madhuran.siva@gmail.com>
2024-10-14 22:38:41 -0400

    fix: Add performant fire animation (#26)
    * add fire animation with lottie
    
    * run prettier
    
    * attempting to setup animation with rive
    
    * feat: added rive fire animation
    
    ---------
    
    Co-authored-by: Arian Ahmadinejad <ahmadinejadarian@gmail.com>

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import useWindowDimensions from "~/hooks/useWindowDimensiosn";
+import { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
 
 const Hero = () => {
   const { width } = useWindowDimensions();
@@ -107,6 +108,19 @@ const Hero = () => {
     }
   }, [width]);
 
+  const { RiveComponent } = useRive({
+    // Load a local riv `clean_the_car.riv` or upload your own!
+    src: "fire.riv",
+    // Be sure to specify the correct state machine (or animation) name
+    stateMachines: "fire",
+    // This is optional.Provides additional layout control.
+    layout: new Layout({
+      fit: Fit.Contain, // Change to: rive.Fit.Contain, or Cover
+      alignment: Alignment.Center,
+    }),
+    autoplay: true,
+  });
+
   return (
     <>
       <div className="relative -z-10 w-full overflow-x-clip">
@@ -127,14 +141,7 @@ const Hero = () => {
           className="relative block w-full lg:hidden"
         />
         <div id="fire" className="absolute" style={fireClass}>
-          <Image
-            unoptimized
-            src={"/static_fire.svg"}
-            alt={"Fire Animation"}
-            width={2000}
-            height={2000}
-            className="ml-2 w-32"
-          />
+          <RiveComponent className="h-48 w-48"></RiveComponent>
         </div>
         <div id="tree-1" className="absolute" style={tree1Class}>
           <Image
