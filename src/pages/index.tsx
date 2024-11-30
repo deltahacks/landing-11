03ddd3814e8cc45056d616c3e93bc860b40a6a3d
Krish120003 <krish120003@gmail.com>
2024-11-30 01:10:42 -0500

    feat: page wide continious snow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,27 +11,29 @@ import Snow from "~/components/Snow";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <main className="flex h-full w-full flex-col gap-24 overflow-hidden">
-        <div className="fixed z-50 box-content w-full">
-          <Navbar />
-        </div>
+    <>
+      <div className="relative">
         <Snow />
-        <Hero />
-        <WhoAreWe />
-        <JoinGrowingCommunity />
-        <Carousel />
-        <Sponsors />
-        <FAQ />
-        <Newsletter />
-        <Footer />
-      </main>
+        <main className="flex h-full w-full flex-col gap-24 overflow-hidden">
+          <div className="fixed z-50 box-content w-full">
+            <Navbar />
+          </div>
+          <Hero />
+          <WhoAreWe />
+          <JoinGrowingCommunity />
+          <Carousel />
+          <Sponsors />
+          <FAQ />
+          <Newsletter />
+          <Footer />
+        </main>
 
-      {/* 
+        {/* 
         positioning & negative index:
         so that we can easily add background images that are on top of the background, but below any other elements like text
       */}
-      <div className="absolute top-0 -z-50 h-full w-full bg-[#E1D1FC]" />
-    </div>
+        <div className="absolute top-0 -z-50 h-full w-full bg-[#E1D1FC]" />
+      </div>
+    </>
   );
 }
