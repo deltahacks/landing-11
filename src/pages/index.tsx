22ce4f8e3e7dce94cdced01c2332af7f7238c6ef
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-12-28 15:33:44 -0500

    fix: snow page overflow (#49)
    * Revert "feat: Snow (#46)"
    
    This reverts commit 5a7496937cc21f11a993e3d481e3aee2ff2c9819.
    
    * fix: snow animation margin issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,33 +7,55 @@ import Hero from "~/sections/Hero";
 import Navbar from "~/components/Navbar";
 import Newsletter from "~/sections/Newsletter";
 import WhoAreWe from "~/sections/WhoAreWe";
-import Snow from "~/components/Snow";
+
+const SnowEffect = () => {
+  // Create 50 snowflakes
+  return (
+    <div className="pointer-events-none fixed inset-0 z-40">
+      {[...Array<number>(50)].map((_, i) => {
+        const size = Math.random() * 8 + 4;
+        const blurDuration = Math.random() * 5 + 12;
+        return (
+          <div
+            key={i}
+            className="snowflake rounded-full bg-white"
+            style={{
+              left: `${Math.random() * 100}%`,
+              width: `${size}px`,
+              height: `${size}px`,
+              animationDelay: `${Math.random() * 10}s`,
+              animationDuration: `${blurDuration}s`,
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <>
-      <div className="relative">
-        <Snow />
-        <main className="flex h-full w-full flex-col gap-24 overflow-hidden">
-          <div className="fixed z-50 box-content w-full">
-            <Navbar />
-          </div>
-          <Hero />
-          <WhoAreWe />
-          <JoinGrowingCommunity />
-          <Carousel />
-          <Sponsors />
-          <FAQ />
-          <Newsletter />
-          <Footer />
-        </main>
+    <div className="relative">
+      <SnowEffect />
+      <main className="flex h-full w-full flex-col gap-24 overflow-hidden">
+        <div className="fixed z-50 box-content w-full">
+          <Navbar />
+        </div>
+        <Hero />
+        <WhoAreWe />
+        <JoinGrowingCommunity />
+        <Carousel />
+        <Sponsors />
+        <FAQ />
+        <Newsletter />
+        <Footer />
+      </main>
 
-        {/* 
+      {/* 
         positioning & negative index:
         so that we can easily add background images that are on top of the background, but below any other elements like text
       */}
-        <div className="absolute top-0 -z-50 h-full w-full bg-[#E1D1FC]" />
-      </div>
-    </>
+      <div className="absolute top-0 -z-50 h-full w-full bg-[#E1D1FC]" />
+    </div>
   );
 }
