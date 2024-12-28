f37fe0413c0a2461950b23da6e0f038a441edf2b
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2024-12-28 14:52:18 -0500

    Revert "feat: Snow (#46)"
    This reverts commit 5a7496937cc21f11a993e3d481e3aee2ff2c9819.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,33 +7,29 @@ import Hero from "~/sections/Hero";
 import Navbar from "~/components/Navbar";
 import Newsletter from "~/sections/Newsletter";
 import WhoAreWe from "~/sections/WhoAreWe";
-import Snow from "~/components/Snow";
 
 export default function Home() {
   return (
-    <>
-      <div className="relative">
-        <Snow />
-        <main className="flex h-full w-full flex-col gap-24 overflow-hidden">
-          <div className="fixed z-50 box-content w-full">
-            <Navbar />
-          </div>
-          <Hero />
-          <WhoAreWe />
-          <JoinGrowingCommunity />
-          <Carousel />
-          <Sponsors />
-          <FAQ />
-          <Newsletter />
-          <Footer />
-        </main>
+    <div className="relative">
+      <main className="flex h-full w-full flex-col gap-24 overflow-hidden">
+        <div className="fixed z-50 box-content w-full">
+          <Navbar />
+        </div>
+        <Hero />
+        <WhoAreWe />
+        <JoinGrowingCommunity />
+        <Carousel />
+        <Sponsors />
+        <FAQ />
+        <Newsletter />
+        <Footer />
+      </main>
 
-        {/* 
+      {/* 
         positioning & negative index:
         so that we can easily add background images that are on top of the background, but below any other elements like text
       */}
-        <div className="absolute top-0 -z-50 h-full w-full bg-[#E1D1FC]" />
-      </div>
-    </>
+      <div className="absolute top-0 -z-50 h-full w-full bg-[#E1D1FC]" />
+    </div>
   );
 }
